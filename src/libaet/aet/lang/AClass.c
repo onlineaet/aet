4f641ee102eebaff0dcd19bd3d78d30daacdb711
onlineaet <zclei@sina.com>
2022-12-04 22:31:33 +0800

    获取接口在类中的偏移。

--- src/libaet/aet/lang/AClass.c
+++ src/libaet/aet/lang/AClass.c
@@ -21,6 +21,20 @@ impl$ AClass{
         return  interfaces;
     }
 
+    public$ unsigned long getInterfaceOffset(int index){
+       if(interfaceCount==0){
+          printf("获取接口在类中的偏移地址出错,类%s没有接口 index:%d\n",name,index);
+          exit(0);
+          return 0;
+       }
+       if(index<0 || index>=interfaceCount){
+           printf("获取接口在类中的偏移地址出错,类%s实现了%d个接口 index:%d\n",name,interfaceCount,index);
+           exit(0);
+          return 0;
+       }
+       return interfacesOffset[index];
+    }
+
 
     AClass(){
         name=NULL;
